93be150d162dd4faf4d5853e4c0f5bb420685c03
Crimson Zero <81848256+Crimson-Zero@users.noreply.github.com>
2022-06-19 20:19:04 +0500

    Add files via upload

---
+++ movie_website.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 19 19:49:29 2022
+
+"""
+
+def Reverse(lst):
+    
+    modified_list = lst[::-1]
+    return (modified_list)
+
+from bs4 import BeautifulSoup
+import requests
+
+response = requests.get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
+
+website = response.text
+soup = BeautifulSoup(website , "html.parser")
+
+find_movie = soup.find_all(name = "h3",class_="title")
+movie_list = []
+
+for movie in find_movie:
+    
+    movie_name = movie.getText()
+    movie_list.append(movie_name)
+
+
+reversed_list = Reverse(movie_list)
+print(reversed_list)
+
+
+for data in reversed_list:
+    with open("movies.txt","a") as file:
+        file.write(data)
+        file.write("\n")
